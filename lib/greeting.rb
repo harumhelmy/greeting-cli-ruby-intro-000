5bca49954fecbfccb22cb5969f962d0d09ebb553
Harum Helmy <harumendhah@gmail.com>
2019-04-04 20:40:44 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,5 @@
-# code the #greeting method here!
+#learn spec/01_greeting_spec.rbcode the #greeting method here!
+def greeting(name)
+  puts 'Hello #{name}. It\'s nice to meet you.'
+  name = gets.chomp
+end
